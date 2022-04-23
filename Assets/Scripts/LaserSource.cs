8bbb0066eaa13225d441ecf92a20094eb38f45fc
Esteban <44073856+ElEstebann@users.noreply.github.com>
2022-04-23 10:47:34 -0700

    Laser Reflections Working

--- Assets/Scripts/LaserSource.cs
+++ Assets/Scripts/LaserSource.cs
@@ -6,7 +6,7 @@ public class LaserSource : MonoBehaviour
 {
 
     private LineRenderer line;
-    public int maxReflections = 10;
+    public int maxReflections = 1;
     
     // Start is called before the first frame update
     void Start()
@@ -24,19 +24,23 @@ public class LaserSource : MonoBehaviour
     }
     */
 
-    void Update()
+    void FixedUpdate()
     {
+        //Set first line point at transform position
         line.SetPosition(0,transform.position);
         Vector3 direction = transform.right;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
-        
         line.positionCount = 1;
         Debug.DrawRay(transform.position, direction, Color.green);
         
-            
-        
-        Ray ray = new Ray(transform.position, transform.forward);
-        for(int i = 1; i < maxReflections +1; i++)
+        //Setup ray and raycast to detect collisions    
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
+        Ray2D ray = new Ray2D(transform.position, direction);
+
+        //For every index in line.position:
+            //Set next position at collision
+            //If collision is not a mirror, break
+            //If no collision: break loop
+        for(int i = 1; i <= maxReflections +1; i++)
         {
             line.positionCount = i+1;
             if(hit.collider)
@@ -44,24 +48,47 @@ public class LaserSource : MonoBehaviour
                 line.SetPosition(i,hit.point);
                 if(hit.collider.tag != "Mirror")
                 {
+                    handleOtherCollision(hit);
                     break;
                 }
                 
-                ray = new Ray(hit.point,Vector3.Reflect(ray.direction,hit.normal));
-                Debug.DrawRay(hit.point, Vector3.Reflect(ray.direction,hit.normal), Color.green);
-                hit = Physics2D.Raycast(hit.point,Vector3.Reflect(ray.direction,hit.normal));
+                
+                Debug.Log(ray.direction);
+                Vector3 hitpoint = hit.point;
+                direction = ray.direction;
+                
+                
+                ray = new Ray2D(hitpoint,Vector3.Reflect(direction,hit.normal));
+                ray = new Ray2D(ray.origin + ray.direction*.1f,ray.direction);
+                Debug.DrawRay(ray.origin,ray.direction);
+                hit = Physics2D.Raycast(ray.origin,ray.direction);
             }
             else
             {
                 
                 
-                line.SetPosition(i,ray.origin + ray.direction);
+                line.SetPosition(i,ray.origin + ray.direction*999);
                 break;
             }
             
         }
            
             
+    }
+
+    //Handle Non-mirror collisions
+    void handleOtherCollision(RaycastHit2D hit)
+    {
+        switch (hit.collider.tag)
+        {
+            case "Player":
+                Debug.Log("Hit Player");
+                break;
+            case "Wall":
+                break;
+            default:
+                break;
+        }
     }
         
     
